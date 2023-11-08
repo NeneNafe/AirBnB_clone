75a4022ab7709dae6695bc475695eb33946562e3
AzorAha1 <wizfaiz@icloud.com>
2023-11-08 23:12:06 +0100

    error solved

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,7 +17,7 @@ class FileStorage:
     
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        key = f"{<obj.__class__.__name.__}.{obj.id}"
+        key = f"{obj.__class__.__name.__}.{obj.id}"
         FileStorage.__objects[key] = obj
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
