3f32361036e5061d798cbcc888866d899d43176a
AzorAha1 <wizfaiz@icloud.com>
2023-11-08 18:31:44 +0100

    betty update

--- models/base_model.py
+++ models/base_model.py
@@ -20,15 +20,15 @@ class BaseModel:
             if 'id' in kwargs:
                 self.id = kwargs['id']
             if 'created_at' in kwargs:
-                self.created_at = kwargs['created_at']
+                self.created_at = datetime.strptime(
+                    kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
             if 'updated_at' in kwargs:
-                self.updated_at = kwargs['updated_at']
+                self.updated_at = datetime.strptime(
+                    kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
         else:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-
-
 
     def __str__(self):
         """str method
