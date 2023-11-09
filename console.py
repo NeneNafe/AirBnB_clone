5ac3c080e6bf38511182dcf7e30615c106dc6719
NeneNafe <nenechiwaya@gmail.com>
2023-11-10 01:00:18 +0200

    updated create command

--- console.py
+++ console.py
@@ -3,6 +3,9 @@
 python command interpreter
 """
 import cmd
+import json
+from models import storage
+from models.base_model import BaseModel
 
 
 class HBNBCommand(cmd.Cmd):
@@ -16,6 +19,21 @@ class HBNBCommand(cmd.Cmd):
     def do_quit(self, *args):
         """Quit command to exit the program\n"""
         return True
+    
+    def do_create(self, line):
+        """Creates a new instance of BaseModel, saves it (to the JSON file"""
+        if line:
+            all_objs = storage.all()
+            if line not in all_objs():
+                print("** class doesn't exist **")
+            else:
+                # creates an instance of the given class
+                model_class = all_objs[line].__class__
+                new_instance = model_class()
+                new_instance.save()
+                print(new_instance.id)
+        else:
+            print("** class name missing **")
 
 
 if __name__ == "__main__":
