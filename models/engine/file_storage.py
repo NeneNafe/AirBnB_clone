f125a8c4575c482c923bf8f22d81cec1ed1fe40d
AzorAha1 <wizfaiz@icloud.com>
2023-11-09 17:23:03 +0100

    serialized __objects to json file to __file_path with the save method, deserializes the json file to __objects with the reload method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,27 +1,38 @@
 #!/usr/bin/python3
 
 import json
-"""Degine a class"""
+import os
+"""Define a class"""
 
 
 class FileStorage:
-    """a class FileStorage that serializes instances to a JSON file 
-    and deserializes JSON file to instance """
-    
+    """
+    a class FileStorage that serializes instances to a JSON file
+    and deserializes JSON file to instance
+    """
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         """return the dict objects"""
         return FileStorage.__objects
-    
+
     def new(self, obj):
         """sets in __objects the obj with key <obj class name>.id"""
-        key = f"{obj.__class__.__name.__}.{obj.id}"
+        key = f"{obj.__class__.__name__}.{obj.id}"
         FileStorage.__objects[key] = obj
+
     def save(self):
         """serializes __objects to the JSON file (path: __file_path)"""
-        data = {}
-        for key, obj in FileStorage.__objects.items():
-            data[key] = obj.to_dict()
-        
+        with open(file=FileStorage.__file_path, mode='w') as file:
+            serializeddata = json.dumps(FileStorage.__objects)
+            file.write(serializeddata)
+
+    def reload(self):
+        """deserialization of the object file to __objects"""
+        if os.path.exists(FileStorage.__file_path):
+            with open(file=FileStorage.__file_path, mode='r') as file:
+                deserializeddata = json.load(file)
+                FileStorage.__objects = deserializeddata
+        else:
+            pass
