383c1d97d40557ec0ec6541d2f5ad1a5ebce37ea
NeneNafe <nenechiwaya@gmail.com>
2023-11-10 14:59:15 +0200

    Update commandd

--- console.py
+++ console.py
@@ -91,6 +91,45 @@ class HBNBCommand(cmd.Cmd):
                     instances.append(str(value))
             print(instances)
 
+    def do_update(self, line):
+        """ Updates an instance based on the class name and id"""
+        cmd_args = line.split(" ")
+
+        if not line:
+            print("** class name missing **")
+        elif cmd_args[0] not in storage.classes():
+            print("** class doesn't exist **")
+        elif len(cmd_args) < 2:
+            print("** instance id missing **")
+        else:
+            key = f"{cmd_args[0]}.{cmd_args[1]}"
+            if key not in storage.all():
+                print("** no instance found **")
+            elif len(cmd_args) < 4:
+                print("** attribute name missing **")
+            elif len(cmd_args) < 5:
+                print("** value missing **")
+            else:
+                instance = storage.all()[key]
+                attribute_name = cmd_args[3]
+                attribue_value = cmd_args[4]
+
+                # checks if the attribute_name is not one that is restricted
+                if attribute_name in ("id", "created_at", "updated_at"):
+                    print("** can't be update id, created_at, upsated_at **")
+                else:
+                    # it gets the attribute type fro the instance
+                    attr_type = type(getattr(instance, attribute_name, None))
+                    # The You cast the attr value to the correct type
+                    try:
+                        casted_value = attr_type(attribue_value)
+                    except ValueError:
+                        print(f"** invalid value for {attribute_name} **")
+                        return
+                    # Then You update the attribute and save the changes
+                    setattr(instance, attribute_name, casted_value)
+                    storage.save()
+
 
 if __name__ == "__main__":
     HBNBCommand().cmdloop()
