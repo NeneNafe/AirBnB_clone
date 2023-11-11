d4820320b167e0111eee905023398a09d9245710
AzorAha1 <wizfaiz@icloud.com>
2023-11-11 05:03:42 +0100

    pycodestyle fix

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -56,5 +56,12 @@ class FileStorage:
         from models.amenity import Amenity
         from models.place import Place
         from models.review import Review
-        class_dict = {'BaseModel': BaseModel, 'User': User, 'State': State, 'Amenity': Amenity, 'Place': Place, 'Review': Review}
+        class_dict = {
+            'BaseModel': BaseModel,
+            'User': User,
+            'State': State,
+            'Amenity': Amenity,
+            'Place': Place,
+            'Review': Review
+            }
         return class_dict
