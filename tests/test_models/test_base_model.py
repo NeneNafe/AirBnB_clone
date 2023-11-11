d4dbfad0a80772bd7258c398b67b9b8fe8ee517d
AzorAha1 <wizfaiz@icloud.com>
2023-11-12 00:30:42 +0100

    update

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,6 +9,8 @@ class TestBaseModel(unittest.TestCase):
     """this is a class that does the test for basemodel"""
     def testBase(self):
         b1 = BaseModel()
+        self.assertTrue(hasattr(b1, 'created_at'))
+        self.assertTrue(hasattr(b1, 'updated_at'))
         self.assertIsInstance(b1.id, str)
         self.assertIsInstance(b1.created_at, datetime)
         self.assertIsInstance(b1.updated_at, datetime)
