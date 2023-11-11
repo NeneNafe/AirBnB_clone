bd93fcade141c4541a324c955efd82360079aac9
AzorAha1 <wizfaiz@icloud.com>
2023-11-12 00:42:26 +0100

    update test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -2,7 +2,7 @@ from models.base_model import BaseModel
 """doing some unittesting for BaseModel"""
 import unittest
 import uuid
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class TestBaseModel(unittest.TestCase):
@@ -11,6 +11,10 @@ class TestBaseModel(unittest.TestCase):
         b1 = BaseModel()
         self.assertTrue(hasattr(b1, 'created_at'))
         self.assertTrue(hasattr(b1, 'updated_at'))
+        delay = timedelta(seconds=1)
+        timediff = datetime.now() - b1.created_at
+        self.assertTrue(timediff < delay)
+        #self.assertAlmostEqual(b1.created_at, datetime.now())
         self.assertIsInstance(b1.id, str)
         self.assertIsInstance(b1.created_at, datetime)
         self.assertIsInstance(b1.updated_at, datetime)
