5c37af8171ea352e1e3bc252bd15ce36bbbc027b
AzorAha1 <wizfaiz@icloud.com>
2023-11-11 13:36:50 +0100

    tests

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,1 +1,14 @@
 from models.base_model import BaseModel
+"""doing some unittesting for BaseModel"""
+import unittest
+import uuid
+from datetime import datetime
+
+
+class TestBaseModel(unittest.TestCase):
+    """this is a class that does the test for basemodel"""
+    def testBase(self):
+        b1 = BaseModel()
+        self.assertIsInstance(b1.id, str)
+        self.assertIsInstance(b1.created_at, datetime)
+        self.assertIsInstance(b1.updated_at, datetime)
