e8b32230ac001667d06850a64c12556448993ba7
AzorAha1 <wizfaiz@icloud.com>
2023-11-12 02:02:45 +0100

    test for file storage

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+import unittest
+from models.engine.file_storage import FileStorage
+
+
+class TestFileStorage(unittest.TestCase):
+    """This is the test for file_storage"""
+    pass
