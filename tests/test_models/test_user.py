834b38cc9a31e64acac0ec22f2f0bf92ffcb0a1f
AzorAha1 <wizfaiz@icloud.com>
2023-11-12 03:39:46 +0100

    test base model

--- tests/test_models/test_user.py
+++ tests/test_models/test_user.py
@@ -5,8 +5,10 @@ from models.user import User
 
 
 class TestUser(unittest.TestCase):
-    """This is the test for User"""
-    pass
+    """This is the test for User class"""
+    def test_user(self):
+        user = User()
+        self.assertIsInstance(user, User)
 
 
 if __name__ == "__main__":
