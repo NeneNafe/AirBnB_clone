21e87f94efa6685cae165183691deb3228f61507
AzorAha1 <wizfaiz@icloud.com>
2023-11-12 03:27:55 +0100

    test user

---
+++ tests/test_models/test_user.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""class that does unittest"""
+import unittest
+from models.user import User
+
+
+class TestUser(unittest.TestCase):
+    """This is the test for file_storage"""
+    pass
+
+
+if __name__ == "__main__":
+    unittest.main()
