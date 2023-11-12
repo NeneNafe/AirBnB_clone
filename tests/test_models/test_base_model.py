897bd3fdf754a171a76356d601a891c482d10a22
AzorAha1 <wizfaiz@icloud.com>
2023-11-12 03:36:38 +0100

    test base model

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -20,11 +20,11 @@ class TestBaseModel(unittest.TestCase):
         self.assertRegex(
             b1.id,
             r'^[0-9a-fA-F]{8}-'
-            r'^[0-9a-fA-F]{4}-'
-            r'^[0-9a-fA-F]{4}-'
-            r'^[0-9a-fA-F]{4}-'
-            r'^[0-9a-fA-F]{12}'
-        )
+            r'[0-9a-fA-F]{4}-'
+            r'[0-9a-fA-F]{4}-'
+            r'[0-9a-fA-F]{4}-'
+            r'[0-9a-fA-F]{12}$'
+            )
 
 
 if __name__ == "__main__":
